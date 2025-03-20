0bb43ff5b56bc0d3b693fc2ca4be1c42e629661c
wiamerz <wiamerm2023@gmail.com>
2025-03-20 20:28:49 +0000

    home page, footer

---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      className={`text-center p-6 mt-7 bg-black `}
+    >
+      <p className={`text-white`}>
+        © 2025 All rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,23 +1,59 @@
 import React from 'react';
 import myImage from "/src/assets/bg.jpg";
+import about from "/src/assets/about.jpg"
 import Navbar from './Navbar';
-function Home() {
+import Footer from './Footer';
+
+
+const Home = () => {
     
     
   return (
     <>
     <Navbar/>
-    <div className="h-screen w-full bg-cover bg-center" style={{ backgroundImage: `url(${myImage})` }}>
+
+
+  <div className="h-screen w-full bg-cover bg-center" style={{ backgroundImage: `url(${myImage})` }}>
     <h1 className="text-white text-4xl font-bold flex justify-center items-center h-full">
     Snozy services for lasting qualite
     </h1>
   </div>
+
+
+{/* -------------------------about u-----------------------*/}
+  <div className='text-2xl m-15 font-bold text-red-600 flex justify-start items-center'>
+     # About us
+  </div>
+ 
+
+ <div className='flex justify-around items-center  '>
+    <div className=' text-black  text-lg w-100 m-15  '>
+       <p>At Snozy we believe in innovation and excellence. Since our inception, 
+        we have been committed to delivering high-quality solutions tailored to our clients' needs.
+    </p>
+       <p>Our Mission: To provide innovative services and products that simplify lives and create value.</p> 
+    <p> Our Vision: To become a leader in our industry by combining expertise and passion.</p>
+    <p>Our Values: Commitment, transparency, and continuous improvement.</p>
+    <p>Our team of dedicated professionals works every day to deliver exceptional experiences and build a better future. </p>
+      
+    </div>
+    <div className='flex justify-end '>
+      <img 
+       src={about}
+        alt="" />
+    </div>
+    
+ </div>
+  
+  {/* -------------------------affichage des produits-----------------------*/}
+
+ <div className='text-2xl m-15 font-bold text-red-600 flex justify-start items-center'>
+     # Projects
+  </div>
+
+<Footer/>
     </>
   )
 }
 
-export default Home
-
-
-
-
+export default Home;
